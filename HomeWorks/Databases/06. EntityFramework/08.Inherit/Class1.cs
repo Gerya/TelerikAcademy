4f0f0d85c7b1d72afe1468327faa1436503f8358
Gerya <gerya@mail.bg>
2013-07-16 10:01:32 +0300

    Entity Framework to 8

---
+++ HomeWorks/Databases/06. EntityFramework/08.Inherit/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Northwind.Model;
+
+namespace Inherit
+{
+    public class EmployeeExtd : Employee
+    {
+        public IEnumerable<Region> Teritory { get;}
+    }
+}
---
+++ HomeWorks/Databases/06. EntityFramework/Northwind.Model/Northwind.Model.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\geri\Documents\SQL Server Management Studio\Projects\EntityFramework\Northwind.Model\Northwind.Model.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.

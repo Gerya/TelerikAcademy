c486dab829be1bf8d9900072c1f16c16adc9589e
Gerya <gerya@mail.bg>
2013-08-09 22:18:33 +0300

    Web services

---
+++ HomeWorks/Web Services/FeedzillaConsumer/FeedzillaConsumer/ArticlesCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FeedzillaConsumer
+{
+    public class ArticlesCollection
+    {
+        public IEnumerable<object> Articles { get; set; }
+    }
+}
---
+++ HomeWorks/Web Services/FeedzillaConsumer/FeedzillaConsumer/FeedzillaConsumer.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+
+namespace FeedzillaConsumer
+{
+    internal class FeedzillaConsumer
+    {
+        static async void PrintArticles(HttpClient httpClient, string queryString)
+        {
+            var response = await httpClient.GetAsync(string.Format("v1/articles/search.json?q={0}", queryString));
+
+            var articlesFound = response.Content.ReadAsStringAsync().Result;
+
+            var data = JsonConvert.DeserializeObject<ArticlesCollection>(articlesFound);
+            
+            Console.WriteLine();
+
+            foreach (var article in data.Articles)
+            {
+                var newsArticle = JsonConvert.DeserializeObject<NewsArticle>(article.ToString());
+                Console.WriteLine(
+                    "Title:\n{0}\nUrl:\n{1}\n",
+                    newsArticle.Title,
+                    newsArticle.Url);
+            }
+        }
+
+        private static void Main()
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://api.feedzilla.com/");
+
+            Console.WriteLine("Query String: ");
+            string queryString = Console.ReadLine();
+
+            PrintArticles(httpClient, queryString);
+            Console.ReadLine();
+        }
+    }
+}
